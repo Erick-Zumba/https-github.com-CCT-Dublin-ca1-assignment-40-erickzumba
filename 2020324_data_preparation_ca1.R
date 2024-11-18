a702867d67bb4f9c2a0ff2976af159c5c0e70f66
Erick Zumba <2020324@student.cct.ie>
2024-11-18 18:13:19 +0000

    first commit - get/set word dir and choosing the data set

---
+++ 2020324_data_preparation_ca1.R
@@ -0,0 +1,6 @@
+#getting the work directory
+getwd()
+#setting the working directory and using the download folder
+setwd("C:/Users/Erick/Downloads")
+#unzip the data that we will work with
+unzip("all_merged.zip", exdir="data")
