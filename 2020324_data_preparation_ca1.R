58e31ccea300d89e0388cc8dd835a684dea239c0
Erick Zumba <2020324@student.cct.ie>
2024-11-19 17:27:54 +0000

    removing any NA values

--- 2020324_data_preparation_ca1.R
+++ 2020324_data_preparation_ca1.R
@@ -6,5 +6,26 @@ setwd("C:/Users/Erick/Downloads")
 #load necessary libraries
 library(dplyr)
 library(tidyr)
+#reading the data set from the csv file
 dataset <- read.csv("Electric_Vehicle_Population_Data.csv")
 head(dataset)
+#summarizing the dataset
+summary(dataset)
+dataset_clean <- na.omit(dataset)
+
+# Remove columns with any NA
+dataset_clean <- dataset[, colSums(is.na(dataset)) == 0]
+
+# Check if there are still NA values
+sum(is.na(dataset_clean))
+
+# Replace NA with mean in numerical columns
+dataset_clean <- dataset %>%
+  mutate(across(where(is.numeric), ~ ifelse(is.na(.), mean(., na.rm = TRUE), .)))
+
+# Check if there are still missing values
+sum(is.na(dataset_clean))
+
+# View cleaned dataset
+head(dataset_clean)
+
