f59575d73ee61248110045ce54fd3d9f0e9c8bbd
Erick Zumba <2020324@student.cct.ie>
2024-11-20 15:33:40 +0000

    scatterplot for outliers-EDA

--- 2020324_data_preparation_ca1.R
+++ 2020324_data_preparation_ca1.R
@@ -6,22 +6,15 @@ setwd("C:/Users/Erick/Downloads")
 #load necessary libraries
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 #reading the data set from the csv file
 dataset <- read.csv("Electric_Vehicle_Population_Data.csv")
 head(dataset)
 #summarizing the dataset
 summary(dataset)
-dataset_clean <- na.omit(dataset)
-
-# Remove columns with any NA
-dataset_clean <- dataset[, colSums(is.na(dataset)) == 0]
-
-# Check if there are still NA values
-sum(is.na(dataset_clean))
 
 # Replace NA with mean in numerical columns
-dataset_clean <- dataset %>%
-  mutate(across(where(is.numeric), ~ ifelse(is.na(.), mean(., na.rm = TRUE), .)))
+dataset_clean <- dataset %>%  mutate(across(where(is.numeric), ~ ifelse(is.na(.), mean(., na.rm = TRUE), .)))
 
 # Check if there are still missing values
 sum(is.na(dataset_clean))
@@ -29,3 +22,10 @@ sum(is.na(dataset_clean))
 # View cleaned dataset
 head(dataset_clean)
 
+# Scatterplot to identify relationships and potential outliers between Electric.Range and Base.MSRP
+ggplot(dataset, aes(x = Electric.Range, y = Base.MSRP)) +
+  geom_point(color = "blue", alpha = 0.6) +
+  theme_minimal() +
+  labs(title = "Scatterplot: Electric Range vs Base MSRP",
+       x = "Electric Range (miles)",
+       y = "Base MSRP ($)")
